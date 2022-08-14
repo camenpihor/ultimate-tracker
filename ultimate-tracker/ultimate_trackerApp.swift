76c02888763a503542e14bf7fa96e829ed84806c
Camen <camenpihor@gmail.com>
2022-08-13 23:10:35 -0400

    Initial Commit

---
+++ ultimate-tracker/ultimate_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ultimate_trackerApp.swift
+//  ultimate-tracker
+//
+//  Created by camen on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ultimate_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
