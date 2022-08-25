40d11050cfce38deb35509eaf687423ac3a36f19
Camen <camenpihor@gmail.com>
2022-08-25 10:04:32 -0700

    Start Game view, start planning

--- ultimate-tracker/models/Game.swift
+++ ultimate-tracker/models/Game.swift
@@ -1,8 +1,30 @@
-//
-//  Game.swift
-//  ultimate-tracker
-//
-//  Created by camen on 8/25/22.
-//
-
 import Foundation
+
+struct Game: Identifiable {
+    let id = UUID()
+    let startedAt: Date
+    let finishedAt: Date
+    let softCap: Date
+    let hardCap: Date
+    let duration: TimeInterval
+    let pausedTotal: TimeInterval = 0
+    let homeScore: Int = 0
+    let awayScore: Int = 0
+    let summary: String
+    let goals: [Goal]
+    let stats: [Stat] = Stats.init()
+    let notes: [Note] = []
+
+    init(startedAt, duration, summary, goals) {
+        self.startedAt = startedAt
+        self.duration = duration
+        self.summary = summary
+        self.goals = goals
+        self.softCap = startedAt.addingTimeInterval(duration)  // is there a rule?
+        self.hardCap = startedAt.addingTimeInterval(duration)  // is there a rule?
+    }
+    
+    func isFinished(at: Date) {
+        // duration - (startedAt + pausedTotal) <= 0
+    }
+}
--- ultimate-tracker/views/TrackerView.swift
+++ ultimate-tracker/views/TrackerView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct TrackerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Tracker View! :)")
+        // 
     }
 }
 
